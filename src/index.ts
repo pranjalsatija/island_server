f83d4fda7c64de61f0ff520664fcb4c2d5b3dd20
Pranjal Satija <me@pranj.co>
2018-10-22 20:02:13 -0500

    remove dashboard

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as config from './config';
 import * as log from './logger';
 
 import express from 'express';
-import { default as ParseDashboard } from 'parse-dashboard';
 import { ParseServer } from 'parse-server';
 import * as path from 'path';
 
@@ -12,32 +11,10 @@ const cloud = path.join(__dirname, 'cloud/main.js');
 const cluster = true;
 const objectIdSize = 16;
 const revokeSessionOnPasswordReset = false;
-
-const dashboardURL = `http://localhost:${config.port}${config.dashboardMountPoint}`;
 const serverURL = `http://localhost:${config.port}${config.mountPoint}`;
 
-// Configure dashboard and Express server.
-const server = express();
-if (config.environment == 'DEV') {
-    log.info(`DEV environment detected. Setting up dashboard at ${dashboardURL}.`);
-    const dashboard = ParseDashboard({
-        // apps: [{ serverURL, appId, masterKey, appName }]
-        apps: [
-            {
-                serverURL,
-                appId: config.appId,
-                masterKey: config.masterKey,
-                appName: config.appName,
-            }
-        ]
-    }, { allowInsecureHTTP: true });
-
-    server.use('/dashboard', dashboard);
-} else {
-    log.info(`Detected NODE_ENV ${config.environment}. Disabling dashboard.`);
-}
-
 // Configure API.
+const server = express();
 const api = new ParseServer({ 
     allowClientClassCreation, 
     appId: config.appId,
--- types/parse-server.d.ts
+++ types/parse-server.d.ts
@@ -1,2 +1,1 @@
 declare module 'parse-server';
-declare module 'parse-dashboard';
